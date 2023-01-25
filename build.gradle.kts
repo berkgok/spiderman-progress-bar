f9d1bd09fe5960a12646527a366222fe2cbc370e
berk.gok <berk.gok@trendyol.com>
2023-01-25 15:09:01 +0300

    version upgrade to support IJ 2023.*

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ repositories {
 // Configure Gradle IntelliJ Plugin
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
-    version.set("2022.2")
+    version.set("2023.*")
     type.set("IU") // Target IDE Platform
 
     plugins.set(listOf("com.intellij.java"))
